0ed72fff285cb830630660f9aa6184d06245a36b
rocket7642 <Connor.leonie@gmail.com>
2022-03-02 17:06:47 -0800

    Basic socket.js construct
    Filled out basic functions

---
+++ src/engine/sockets/socket.js
@@ -0,0 +1,26 @@
+"use strict"; 
+
+class Socket{
+
+    constructor(ip, port){ 
+        this.ip = ip;
+        this.port = port;
+
+    }
+
+    init(){ 
+        
+    }
+
+    update(){ 
+
+    }
+
+    getInfo(){ 
+    
+    }
+
+    recieveInfo(){ 
+
+    }
+}
