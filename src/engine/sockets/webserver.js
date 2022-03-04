13f43000cccf29ebedc8dd26a517a04b162cfed7
Vonce <vincerosane@outlook.com>
2022-03-03 21:39:01 -0800

    Update webserver.js

--- src/engine/sockets/webserver.js
+++ src/engine/sockets/webserver.js
@@ -1,14 +1,20 @@
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8080 });
-
+let sockets = [];
+let userID = 0;
 wss.on("connection", (ws, req) => {
+  sockets[userID] = ws;
+  userID++;
   console.log(
-    "Connection made with " + req.headers["x-forwarded-for"] ||
-      req.socket.remoteAddress
+    "Connection made with userID" +
+      userID +
+      " at " +
+      new Date().toLocaleTimeString()
   );
 
   ws.on("message", (message) => {
-    console.log("Received: " + message);
-    ws.send(message);
+    for (let socket of sockets) {
+      socket.send(message);
+    }
   });
 });
